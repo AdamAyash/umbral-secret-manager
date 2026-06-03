111412cd9896a474100c48672ff1a842599a89da
aayash <aayash@mail.com>
2026-06-03 13:28:21 +0300

    Theme

--- frontend/Umbral.WebPortal/src/app/layout/main-layout/main-layout.component.ts
+++ frontend/Umbral.WebPortal/src/app/layout/main-layout/main-layout.component.ts
@@ -4,11 +4,11 @@ import { BadgeModule } from 'primeng/badge';
 import { MenubarModule } from 'primeng/menubar';
 import { InputTextModule } from 'primeng/inputtext';
 import { RippleModule } from 'primeng/ripple';
-import { MenuItem } from 'primeng/api';
+import { ButtonModule } from 'primeng/button'
 
 @Component({
   selector: 'umbral-main-layout',
-  imports: [AvatarModule, BadgeModule, MenubarModule, InputTextModule, RippleModule],
+  imports: [AvatarModule, BadgeModule, MenubarModule, InputTextModule, RippleModule, ButtonModule],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css',
 })
